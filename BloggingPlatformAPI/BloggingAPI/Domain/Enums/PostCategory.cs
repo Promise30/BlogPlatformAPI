2d2ab36cfa8acdb8dece0e58fb2324c653618928
Promise30 <promiseadeagbo@gmail.com>
2024-06-15 17:08:56 +0000

    task: added post category enumeration types

---
+++ BloggingPlatformAPI/BloggingAPI/Domain/Enums/PostCategory.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+
+namespace BloggingAPI.Domain.Enums
+{
+    public enum PostCategory : int
+    {
+        [Description("Entertainment")]
+        Entertainment = 0,
+        [Description("Technology")]
+        Technology = 1,
+        [Description("Science")]
+        Science = 2,
+        [Description("Fashion")]
+        Fashion = 3,
+        [Description("Arts")]
+        Arts = 4,
+        [Description("Sports")]
+        Sports = 5,
+        [Description("News")]
+        News  = 6,
+        [Description("Health")]
+        Health = 7,
+        [Description("Travel")]
+        Travel = 8,
+        [Description("Food")]
+        Food = 9,
+        [Description("Lifestyle")]
+        Lifestyle = 10,
+        [Description("Education")]
+        Education = 11,
+        [Description("Politics")]
+        Politics = 12,
+
+
+    }
+}
