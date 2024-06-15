bb17e6e3ea448f324d66bbc5fa0e681fab536ca2
Promise30 <promiseadeagbo@gmail.com>
2024-06-15 17:12:12 +0000

    task: added email service related features

---
+++ BloggingPlatformAPI/BloggingAPI/Constants/EmailResponseMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace BloggingAPI.Constants
+{
+    public static class EmailResponseMessage
+    {
+        public static string GetEmailSuccessMessage(string emailAddress) => $"Email sent successfully to {emailAddress}";
+        
+    }
+}
