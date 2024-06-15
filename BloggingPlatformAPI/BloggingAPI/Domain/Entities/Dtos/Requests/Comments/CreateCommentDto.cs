b55df5ad57700ef67020349c0a8858b73e243cd4
Promise30 <promiseadeagbo@gmail.com>
2024-06-15 17:17:52 +0000

    task: added comment related dtos

---
+++ BloggingPlatformAPI/BloggingAPI/Domain/Entities/Dtos/Requests/Comments/CreateCommentDto.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BloggingAPI.Domain.Entities.Dtos.Requests.Comments
+{
+    public record CreateCommentDto(
+    [Required(ErrorMessage ="Content field is required")]
+    string Content);
+}
---
+++ BloggingPlatformAPI/BloggingAPI/Domain/Entities/Dtos/Requests/Comments/UpdateCommentDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BloggingAPI.Domain.Entities.Dtos.Requests.Comments
+{
+    public record UpdateCommentDto
+    (
+        [Required(ErrorMessage ="Content field is required")]
+        string Content
+    );
+}
