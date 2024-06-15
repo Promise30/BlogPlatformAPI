c064a4f952fd33ca346098cb94ceb9157096ec58
Promise30 <promiseadeagbo@gmail.com>
2024-06-15 17:04:47 +0000

    task: added project migrations

---
+++ BloggingPlatformAPI/BloggingAPI/Migrations/20240608143226_ChangedTypeOfCategoryForPostModelToEnum.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BloggingAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedTypeOfCategoryForPostModelToEnum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Category",
+                table: "BlogPosts",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RefreshToken",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+           
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Category",
+                table: "BlogPosts",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RefreshToken",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            
+        }
+    }
+}
---
+++ BloggingPlatformAPI/BloggingAPI/Migrations/20240614173557_AddedImageRelatedPropertiesToPostModel.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BloggingAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedImageRelatedPropertiesToPostModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageFormat",
+                table: "BlogPosts",
+                type: "nvarchar(max)",
+                nullable: true,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePublicId",
+                table: "BlogPosts",
+                type: "nvarchar(max)",
+                nullable: true,
+                defaultValue: "");
+
+           
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+           
+
+            migrationBuilder.DropColumn(
+                name: "ImageFormat",
+                table: "BlogPosts");
+
+            migrationBuilder.DropColumn(
+                name: "ImagePublicId",
+                table: "BlogPosts");
+
+            
+        }
+    }
+}
