28bc173d6331613fbb8eb6efd767ebc633319839
Promise30 <promiseadeagbo@gmail.com>
2024-06-15 17:18:09 +0000

    task: added post related request dtos

---
+++ BloggingPlatformAPI/BloggingAPI/Domain/Entities/Dtos/Requests/Posts/CreatePostDto.cs
@@ -0,0 +1,22 @@
+﻿
+
+using BloggingAPI.Domain.Enums;
+using BloggingAPI.Infrastructure.Validations;
+using System.ComponentModel.DataAnnotations;
+
+namespace BloggingAPI.Domain.Entities.Dtos.Requests.Posts
+{
+    public class CreatePostDto
+    {
+        [Required(ErrorMessage = "A post title is required")]
+        public string Title { get; init; }
+        [Required(ErrorMessage = "A post content is required")]
+        public string Content { get; init; }
+        [Required(ErrorMessage = "A post category is required")]
+        [ValidPostCategory(ErrorMessage = "Invalid post category type")]
+        public string Category { get; set; }
+        [AllowedFileExtensions(new[]{".jpg", ".png", ".jpeg"}, ErrorMessage ="Only JPG, JPEG and PNG files are allowed")]
+        [MaxFileSize(2 * 1024 * 1024, ErrorMessage = "File size must not exceed 2 MB")]
+        public IFormFile? PostCoverImage { get; init; }
+    }
+}
---
+++ BloggingPlatformAPI/BloggingAPI/Domain/Entities/Dtos/Requests/Posts/UpdatePostDto.cs
@@ -0,0 +1,23 @@
+﻿
+
+using BloggingAPI.Domain.Enums;
+using BloggingAPI.Infrastructure.Validations;
+using System.ComponentModel.DataAnnotations;
+
+namespace BloggingAPI.Domain.Entities.Dtos.Requests.Posts
+{
+    public record UpdatePostDto
+    {
+        [Required(ErrorMessage ="Title field is required")]
+        public string Title { get; init; }
+        [Required(ErrorMessage ="Content field is required")]
+        public string Content { get; init; }
+        [Required(ErrorMessage = "Category field is required")]
+        [ValidPostCategory(ErrorMessage = "Invalid post category type")]
+        public string Category { get; set; }
+        [AllowedFileExtensions(new[] { ".jpg", ".png", ".jpeg" }, ErrorMessage = "Only JPG, JPEG and PNG files are allowed")]
+        [MaxFileSize(2 * 1024 * 1024, ErrorMessage = "File size must not exceed 2 MB")]
+        public IFormFile? PostCoverImage { get; init; }
+    }
+
+}
